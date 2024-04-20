af093431562d6a46acb333579d7ba09324ff3458
dianamelnikova1 <123232139+dianamelnikova1@users.noreply.github.com>
2024-04-20 22:38:38 +0500

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+mod quick_sort;
+mod select_sort;
+mod insert_sort;
+mod merge_sort;
+
+pub use quick_sort::quick_sort;
+pub use select_sort::select_sort;
+pub use insert_sort::insert_sort;
+pub use merge_sort::merge_sort;
